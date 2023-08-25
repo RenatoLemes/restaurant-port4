d301d93597ac40c664c2c69d6f1d352a5ee73372
RenatoLemes <renato.lemmes@hotmail.com>
2023-08-25 12:10:49 +0000

    Added reservations views.py and models.py and a form.

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Table(models.Model):
+    table_number = models.CharField(max_length=10)
+    capacity = models.PositiveIntegerField()
+
+class Reservation(models.Model):
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    date_time = models.DateTimeField()
+    guest_name = models.CharField(max_length=100)
+    guest_count = models.PositiveIntegerField()
+    canceled = models.BooleanField(default=False)
--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Table, Reservation
+from .forms import ReservationForm
 
-# Create your views here.
+
+def make_reservation(request):
+    if request.method == 'POST':
+        form = ReservationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('reservation_success')  # Create this URL/view for the success page
+    else:
+        form = ReservationForm()
+    return render(request, 'make_reservation.html', {'form': form})
